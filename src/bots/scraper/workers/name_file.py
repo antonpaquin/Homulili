634eb66b5aacf74eaad7ebe92f72165bb7b45b16
Anton Paquin <antonpaquin@gmail.com>
2017-11-14 13:59:51 -0500

    Scraper print logs were too verbose

--- src/bots/scraper/workers/name_file.py
+++ src/bots/scraper/workers/name_file.py
@@ -12,7 +12,6 @@ root_dir = fs.path.join('/data', 'raw_files/')
 
 @input_protection()
 def name_file(input: File, output: Queue):
-    print('Enter name_file')
     save_dir = fs.path.join(root_dir, str(input.manga_id))
     filename = '{file_id}_-_{remote_name}'.format(
         file_id=input.file_id,
